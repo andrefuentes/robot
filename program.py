f4aa703dbcb0b6b204d4915a0c050d48c85e2246
andrefuentes <andrefuentes166@gmail.com>
2016-08-25 14:47:46 -0600

    program

--- program.py
+++ program.py
@@ -1,9 +1,21 @@
+import time
+mapa_ancho=80
+mapa_largo=25
+def imprimir_robot(x,y):
+	r=(" " * cargar_mapa)*y
+	r+=" " * x +" * "
+	r+=" "+cargar_mapa*(mapa_largo+(y+1))
+	return(r)
+steps=19
+for i in range(steps)
+	time.sleep(0.3)
+	print (imprimir_robot(5+i,i))
 def cargar_instruciones(nombre_archivo):
 	lista = []
 	ins_file = open(nombre_archivo)
 	for linea in ins_file: 
 		lista.append(list(linea.strip()))
-	print (lista)
+	return (lista)
 	
 
 
--- robot.py
+++ robot.py
@@ -5,15 +5,15 @@ class Robot():
 		self.x=0
 		self.fichas=0
 		self.direccion=0
-		self.mapa=caragr_mapa		
+		self.mapa=caragar_mapa		
 	def mover(self):
 		if self.direccion==0 and self.x<80:
 			self.x=1
-		if self.direccion==0 and self.y>25:
+		if self.direccion==0 and self.y<25:
 			self.y=1
 	def pick(self):
 		for x in len(mapa.lista):
-			self.fichas=fichas+1
+
 		return fichas
 
 
