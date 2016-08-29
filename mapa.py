3071faea44e085b8e987cd2d01e022105221dae4
andrefuentes <andrefuentes166@gmail.com>
2016-08-29 14:48:13 -0600

    mejoras

--- mapa.py
+++ mapa.py
@@ -1,6 +1,3 @@
 import ruplay
 m=input()
 print (ruplay.cargar_mapa(m))
-class Mapa (object):
-	def __init__ (self,ancho,alto):
-		
--- robot.py
+++ robot.py
@@ -1,3 +1,4 @@
+import time
 class Robot(object):
 	def __init__(self,x,y):
 		self.monedas=monedas
@@ -10,17 +11,25 @@ class Robot(object):
 		self.mapa=mapa
 	def mover(self):
 		if self.direccion==0 and self.x<80:
-			self.x=1
+			self.x+=1
 		if self.direccion==80 and self.y<25:
-			self.y=1
-		if self.direcion==
+			self.y+=1
+		if self.direccion==80 and self.y<25:
+			self.x-=1
+		if self.direccion==0 and self.x<80:
+			self.y-=1
 	def rotar(self):
-
-
+		if self.direccion="^"	
+			return "^"
+		elif self.direccion=">"
+			return ">"
+		elif self.direccion="<"
+			return "<"
+		elif self.direccion="v"
+			return "v"
 	def pick(self):
 		for x in len(mapa.lista):
-
-		return fichas
+		return monedas
 class Mapa(object):
 	def __init__ (self,ancho,alto):
 		self.ancho=ancho
@@ -30,19 +39,28 @@ class Mapa(object):
 	def asignar_robot(self,robot):
 		self.robot=robot
 	def agregar_monedas(self,monedas):
-		
+		return monedas
+mapa_ancho=80
+mapa_largo=25
+def imprimir_robot(x,y):
+	r=(" " * cargar_mapa)*y
+	r+=" " * x +" * "
+	r+=" "+cargar_mapa*(mapa_largo+(y+1))
+	return(r)
+steps=19
+for i in range(steps)
+	time.sleep(0.3)
+	print (imprimir_robot(5+i,i))
+def cargar_instruciones(nombre_archivo):
+	lista = []
+	ins_file = open(nombre_archivo)
+	for linea in ins_file: 
+		lista.append(list(linea.strip()))
+	return (lista)
 class Monedas (object):
 	def __init__ (self,x,y):
 		self.x=x
 		self.y=y
-
-
-
-
-
-
-
-
 <
 >
 v
