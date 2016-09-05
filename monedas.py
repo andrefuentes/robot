3b0f1af34968c1e9a3c92e0984cf73323376535f
andrefuentes <andrefuentes166@gmail.com>
2016-09-05 14:17:24 -0600

    mejoras3.0

--- monedas.py
+++ monedas.py
@@ -2,12 +2,3 @@ class Monedas (object):
 	def __init__ (self,x,y):
 		self.x=x
 		self.y=y
-	def imprimir_robot(x,y):
-		r=(" " * cargar_mapa)*y
-		r+=" " * x +" * "
-		r+=" "+cargar_mapa*(mapa_largo+(y+1))
-		return(r)
-		steps=19
-		for i in range(steps)
-		time.sleep(0.3)
-		print (imprimir_robot(5+i,i))
