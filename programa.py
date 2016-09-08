098c3fb75bbe2f3eebe05f0fbafbd2fc7c97ea70
andrefuentes <andrefuentes166@gmail.com>
2016-09-08 14:48:52 -0600

    mejora
    ahi vamos

---
+++ programa.py
@@ -0,0 +1,22 @@
+import time
+mapa_ancho=80
+mapa_largo=25
+def imprimir_robot(x,y):
+	r=(" " * cargar_mapa)*y
+	r+=" " * x +" * "
+	r+=" "+cargar_mapa*(mapa_largo+(y+1))
+	return(r)
+steps=19
+for i in range(steps)
+	time.sleep(0.3)
+	print (imprimir_robot(5+i,i))
+	def imprimir_robot(x,y):
+		r=(" " * cargar_mapa)*y
+		r+=" " * x +" * "
+		r+=" "+cargar_mapa*(mapa_largo+(y+1))
+		return(r)
+		steps=19
+		for i in range(steps)
+		time.sleep(0.3)
+		print (imprimir_robot(5+i,i))
+		
